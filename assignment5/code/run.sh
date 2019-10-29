86232cfa11686c7f67ed3acd17da59b67767a667
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2019-10-29 23:47:48 +0530

    Completed P5

--- assignment5/code/run.sh
+++ assignment5/code/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#Uncomment this the first time you run it:-
+
+#wget -r -nH --cut-dirs=2 --no-parent --reject="index.html*" https://www.cse.iitm.ac.in/~krishna/cs3300/microIR/
+
 javac P5.java
 
-for file in ./microIR_tests/*
+for file in ./microIR/*
 do
 	java P5 < $file >dump
 	echo "Testing $file"
